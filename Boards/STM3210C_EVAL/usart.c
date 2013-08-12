0ae53d1bb2924db571f0e4641161d0fc61866af7
Alexey Dyachenko <alexd@inbox.ru>
2013-08-12 16:52:28 +0200

    usart init without stm library, part 1 -- usart and remap

--- Boards/STM3210C_EVAL/usart.c
+++ Boards/STM3210C_EVAL/usart.c
@@ -29,17 +29,25 @@ int usart_getchar()
   return (char) (USART2->DR & (uint16_t) 0x01FF);
 }
 
-void init_usart( int speed )
+const unsigned int PCLK_FREQUENCY = 36000000UL;
+
+void init_usart( int baudrate )
 {
-	USART_InitTypeDef USART_InitStructure;
   GPIO_InitTypeDef GPIO_InitStructure;
 
+  USART2->CR1 &= ~USART_CR1_UE;
+
   /* Enable GPIO clock */
-  RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOD | RCC_APB2Periph_AFIO, ENABLE );
+/*  RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOD | RCC_APB2Periph_AFIO, ENABLE );*/
+  RCC->APB2ENR |= RCC_APB2ENR_AFIOEN | RCC_APB2ENR_IOPDEN;
 
     /* Enable the USART2 Pins Software Remapping */
-  GPIO_PinRemapConfig( GPIO_Remap_USART2, ENABLE );
-  RCC_APB1PeriphClockCmd( RCC_APB1Periph_USART2, ENABLE );
+/*  GPIO_PinRemapConfig( GPIO_Remap_USART2, ENABLE );*/
+
+  AFIO->MAPR |= AFIO_MAPR_USART2_REMAP;
+
+/*  RCC_APB1PeriphClockCmd( RCC_APB1Periph_USART2, ENABLE );*/
+  RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
 
   /* Configure USART Tx as alternate function push-pull */
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
@@ -52,15 +60,14 @@ void init_usart( int speed )
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
   GPIO_Init( GPIOD, &GPIO_InitStructure );
 
-  USART_InitStructure.USART_BaudRate = speed;
-  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-  USART_InitStructure.USART_StopBits = USART_StopBits_1;
-  USART_InitStructure.USART_Parity = USART_Parity_No;
-  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-  /* USART configuration */
-  USART_Init( USART2, &USART_InitStructure );
+  // no flow control
+  USART2->CR3 = 0;
+  // one stop bit
+  USART2->CR2 = 0;
+  // 8 bit word length, no parity, no interrupts,  enable RX and TX
+  USART2->CR1 = USART_CR1_RE | USART_CR1_TE;
+  // calculate baudrate register
+  USART2->BRR = ( PCLK_FREQUENCY + baudrate / 2 ) / baudrate;
 
   /* Enable the  USART by setting the UE bit in the CR1 register */
   USART2->CR1 |= USART_CR1_UE;
